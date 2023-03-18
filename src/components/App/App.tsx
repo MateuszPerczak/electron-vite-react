60c96557d6576dbf4467741348d465088122c00f
Mateusz Perczak <mateusz.perczak.mp@gmail.com>
2023-03-18 21:07:07 +0100

    Hello electron

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,9 +4,7 @@ import { FC } from "react";
 const App: FC = (): JSX.Element => {
   return (
     <main>
-      <motion.div initial={{ scale: 0, x: -100 }} animate={{ scale: 1, x: 0 }}>
-        HELLO SOURCE MAPS
-      </motion.div>
+      <span>HELLO ELECTRON 🤡</span>
     </main>
   );
 };
